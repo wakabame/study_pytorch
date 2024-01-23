51ef7df52958d00af8cdb3698f6236def9489a0a
wakabame <wakabame89@gmail.com>
2024-01-23 13:39:30 +0000

    plot 関数を準備する

---
+++ study_pytorch/plot_util.py
@@ -0,0 +1,40 @@
+from typing import Any
+
+import numpy as np
+from nptyping import Float, NDArray
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+
+def plot_decision_regions(
+    X: NDArray[Any, Float], y: NDArray[Any, Float], classifier: Any, resolution: float = 0.02
+) -> None:
+    makers = ("o", "s", "^", "v", "<")
+    colors = ("red", "blue", "lightgreen", "gray", "cyan")
+    cmap = ListedColormap(colors[: len(np.unique(y))])
+
+    # 決定領域のプロット
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # グリッドポイントの生成
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
+    # 各特徴量を1次元配列に変換して予測を実行
+    lab = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    # 予測結果を元のグリッドポイントのデータサイズに変換
+    lab = lab.reshape(xx1.shape)
+    # グリッドポイントの等高線をプロット
+    plt.contourf(xx1, xx2, lab, alpha=0.3, cmap=cmap)  # 軸の範囲の設定
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # クラスごとに訓練データをプロット
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(
+            x=X[y == cl, 0],
+            y=X[y == cl, 1],
+            alpha=0.8,
+            c=colors[idx],
+            marker=makers[idx],
+            label=f"Class: {cl}",
+            edgecolor="black",
+        )
